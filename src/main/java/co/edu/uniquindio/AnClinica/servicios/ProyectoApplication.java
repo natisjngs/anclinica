22b7e2b31dd7f797b34232d60867d4922d414414
Juanes-Quin <juanquinrod@gmail.com>
2023-09-29 19:42:46 -0500

    App

--- src/main/java/co/edu/uniquindio/AnClinica/servicios/ProyectoApplication.java
+++
@@ -1,9 +0,0 @@
-package co.edu.uniquindio.AnClinica.servicios;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-@SpringBootApplication
-public class ProyectoApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ProyectoApplication.class, args);
-    }
-}
